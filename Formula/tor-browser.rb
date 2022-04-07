3e89250c6e626394c5b2f453532f4dafd4f353e0
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-04-08 00:14:24 +0800

    Create tor-browser.rb

---
+++ Formula/tor-browser.rb
@@ -0,0 +1,33 @@
+class TorBrowser < Formula
+  desc "Modified Firefox with privacy add-ons, encryption & advanced proxy"
+  homepage "https://www.torproject.org/projects/torbrowser.html.en"
+  version "7.0.10"
+
+  url "https://www.torproject.org/dist/torbrowser/#{version}/tor-browser-linux64-#{version}_en-US.tar.xz"
+  sha256 "10eebffe22594d336441ed59e5edc97ba1d296eb7d94bca3ff94ebfac2da3e34"
+
+  bottle :unneeded
+
+  def install
+    libexec.install Dir["*"]
+    (libexec/"tor-browser_brew_exec").write <<~EOS
+      #!/usr/bin/env bash
+      cd #{libexec}
+      ./start-tor-browser.desktop "$@"
+    EOS
+    chmod(0755, "#{libexec}/tor-browser_brew_exec")
+    bin.install_symlink("#{libexec}/tor-browser_brew_exec" => "tor-browser")
+  end
+
+  def caveats; <<~EOS
+    Executable is linked as "tor-browser".
+    For more usage information, see "tor-browser --help".
+    EOS
+  end
+
+  test do
+    cd(libexec) do
+      assert_equal "Launching './Browser/start-tor-browser --detach --verbose --version'...\nMozilla Firefox 52.5.0\n", shell_output("#{bin}/tor-browser --verbose --version 2>&1")
+    end
+  end
+end
