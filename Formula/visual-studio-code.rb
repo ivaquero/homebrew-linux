5806f8c83c3a700877326ed3516d32d343044d83
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-04-07 23:31:29 +0800

    Update visual-studio-code.rb

--- Formula/visual-studio-code.rb
+++ Formula/visual-studio-code.rb
@@ -3,7 +3,7 @@ class VisualStudioCode < Formula
     homepage "https://code.visualstudio.com"
     version "1.66.0"
 
-    url "https://vscode-update.azurewebsites.net/#{version}/linux-x64/stable"
+    url "https://github.com/microsoft/vscode/archive/refs/tags/#{version}.tar.gz"
     sha256 "97bc49f68a35d8f22fe2706a09ba62e90f2995f4c4f23beb7091e8f9a7cdeb0d"
 
     def install
