f26d021e5d31908836c939b0a8fc887f07aef4a4
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-04-08 00:18:11 +0800

    Create calibre.rb

---
+++ Formula/calibre.rb
@@ -0,0 +1,21 @@
+class Calibre < Formula
+  desc "A powerful and easy to use e-book manager"
+  homepage "https://calibre-ebook.com/"
+  version "5.6.0"
+
+  url "https://github.com/kovidgoyal/calibre/releases/download/v#{version}/calibre-#{version}-x86_64.txz"
+  sha256 "37715ef44a75d4ae7cf2fc9a379805837a29b44f1eeacfab853a0b4acd73157a"
+
+  bottle :unneeded
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink("#{libexec}/calibre" => "calibre")
+  end
+
+  def caveats; <<~EOS
+    Executable is linked as "calibre".
+    For more usage information, see "calibre --help".
+    EOS
+  end
+end
