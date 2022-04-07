a35c07ccb68ca874edb176a1d65d285c1be6f4a8
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-04-08 00:18:56 +0800

    Create firefox.rb

---
+++ Formula/firefox.rb
@@ -0,0 +1,22 @@
+class Firefox < Formula
+    desc "The 100% fresh, free-range, ethical browser, with more speed, privacy and freedom. This is the stable version, in en-US."
+    homepage "https://www.mozilla.org/en-US/firefox/all/"
+    version "63.0.3"
+
+    url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}/linux-x86_64/en-US/firefox-#{version}.tar.bz2"
+    sha256 "0273b63e8bd2901fef31971f0e422e943e04ea86f647e175473bfc668e326bbd"
+
+    conflicts_with "firefox-zhcn"
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/firefox" => "firefox-browser")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "firefox-browser".
+        For more usage information, see "firefox-browser --help".
+        EOS
+    end
+end
