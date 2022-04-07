678186d3f15b1af42ab2679e53543b310b352c6f
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-04-08 00:47:54 +0800

    Update calibre.rb

--- Formula/calibre.rb
+++ Formula/calibre.rb
@@ -1,7 +1,7 @@
 class Calibre < Formula
   desc "A powerful and easy to use e-book manager"
   homepage "https://calibre-ebook.com/"
-  version "5.6.0"
+  version "5.40.0"
 
   url "https://github.com/kovidgoyal/calibre/releases/download/v#{version}/calibre-#{version}-x86_64.txz"
   sha256 "37715ef44a75d4ae7cf2fc9a379805837a29b44f1eeacfab853a0b4acd73157a"
