adde2fd2467afd1fde843646f6af72c6f6441922
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-04-30 20:06:54 +0800

    add clion

---
+++ Formula/clion.rb
@@ -0,0 +1,19 @@
+class Clion < Formula
+    desc "A cross-platform IDE for C and C++"
+    homepage "http://www.jetbrains.com/clion/"
+    version "2018.3.4"
+    url "https://download.jetbrains.com/cpp/CLion-#{version}.tar.gz"
+    sha256 "963fb343272e5903ac7dc944cc64ea9541ab4c150cc4ea796dcb0fb613bff4fd"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/bin/clion.sh" => "clion")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "clion".
+        EOS
+    end
+end
---
+++ Formula/pycharm.rb
@@ -0,0 +1,19 @@
+class Pycharm < Formula
+    desc "Python IDE for Professional Developers, professional edition."
+    homepage "http://www.jetbrains.com/pycharm/"
+    version "2018.1.4"
+    url "https://download.jetbrains.com/python/pycharm-professional-#{version}.tar.gz"
+    sha256 "6cda9b2717b161387983a09ed88b39d76d88fea208601d125666c02b7c6961cf"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/bin/pycharm.sh" => "pycharm")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "pycharm".
+        EOS
+    end
+end
