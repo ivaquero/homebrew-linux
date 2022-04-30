888dd2101f43fe208eb25b62357229744df93b77
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-04-30 20:09:39 +0800

    fix

--- Formula/calibre.rb
+++ Formula/calibre.rb
@@ -1,11 +1,10 @@
 class Calibre < Formula
   desc "A powerful and easy to use e-book manager"
   homepage "https://calibre-ebook.com/"
-  version "5.40.0"
+  version "5.41.0"
 
   url "https://github.com/kovidgoyal/calibre/releases/download/v#{version}/calibre-#{version}-x86_64.txz"
   sha256 "c341796af47b608f988b2bbd5ee38b10f556c2115234731d1f612a17a396b128"
-
   def install
     libexec.install Dir["*"]
     bin.install_symlink("#{libexec}/calibre" => "calibre")
