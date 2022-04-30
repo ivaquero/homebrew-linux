5b1c7f5a6abefc9a342f72f84dcc2b4cdc46b7d0
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-04-30 20:41:47 +0800

    fix

--- Formula/clash-for-windows.rb
+++ Formula/clash-for-windows.rb
@@ -1,10 +1,10 @@
 class ClashForWindows < Formula
     desc "A Windows/macOS/Linux GUI based on Clash and Electron"
     homepage "https://github.com/Fndroid/clash_for_windows_pkg"
-    version "0.19.14"
+    version "0.19.17"
 
     url "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/#{version}/Clash.for.Windows-#{version}-x64-linux.tar.gz"
-    sha256 "b0be6a36830ab18ee1545c5e997626736725abf7a1fe46b5c01343c028c6e512"
+    sha256 "d52b76f2e9289b6cb18e88f710d11b5a0e045992636694b7aec6b7168e5150ba"
 
     def install
         libexec.install Dir["*"]
--- Formula/dbeaver-ee.rb
+++ Formula/dbeaver-ee.rb
@@ -4,7 +4,7 @@ class DbeaverEe < Formula
     version "22.0.0"
 
     if Hardware::CPU.intel?
-        sha256 "b9b722ab0c5ac6099092c43a04426e84bd9b857e646a1f7fe987fb09efdeffec"
+        sha256 "3be04dd98fbb17948e90553b9d0d277fca6505cb9c3fdee9e62fd8a930932835"
     else
         sha256 "b9b722ab0c5ac6099092c43a04426e84bd9b857e646a1f7fe987fb09efdeffec"
     end
