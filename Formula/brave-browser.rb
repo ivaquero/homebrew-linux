fd46ebad7d741c4ff1e42c29135315162f6adb60
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-04-30 21:15:18 +0800

    fix

--- Formula/brave-browser.rb
+++ Formula/brave-browser.rb
@@ -1,9 +1,9 @@
 class BraveBrowser < Formula
     desc "Next generation Brave browser for macOS, Windows, Linux, and eventually Android, based on Chromium."
     homepage "https://github.com/brave/brave-browser"
-    version "0.57.18"
+    version "1.38.109"
 
-    url "https://github.com/brave/brave-browser/releases/download/v#{version}/brave-v#{version}-linux-x64.zip"
+    url "https://github.com/brave/brave-browser/releases/download/v#{version}/brave-#{version}-linux-amd64.zip"
     sha256 "3bcff0a481b1b3f2ac19c2c0363d6b5ce0edfaa219cd0311ace53fbec25487fa"
 
     def install
