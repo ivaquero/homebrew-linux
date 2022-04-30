f1ca5d9863f1de1b7cb64454161d5ce6deab26be
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-04-30 19:26:11 +0800

    add pycharm-ce

---
+++ Formula/pycharm-ce.rb
@@ -0,0 +1,19 @@
+class PycharmCe < Formula
+    desc "Python IDE for Professional Developers, community edition."
+    homepage "http://www.jetbrains.com/pycharm/"
+    version "2018.1.4"
+    url "https://download.jetbrains.com/python/pycharm-community-#{version}.tar.gz"
+    sha256 "90953ca424bb331348e3575975fab4e189ab126ddc367223e3a7cb01b1563f3b"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/bin/pycharm.sh" => "pycharmc")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "pycharmc".
+        EOS
+    end
+end
