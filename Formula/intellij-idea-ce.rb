59c93ff24df6f1ff2c81b5f29b40311c230ae71e
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-04-30 20:04:28 +0800

    add intellij-idea

---
+++ Formula/intellij-idea-ce.rb
@@ -0,0 +1,19 @@
+class IntellijIdeaCe < Formula
+    desc "The Most Intelligent Java IDE, community edition."
+    homepage "http://www.jetbrains.com/idea/"
+    version "2018.1.4"
+    url "https://download.jetbrains.com/idea/ideaIC-#{version}.tar.gz"
+    sha256 "26e674de05976cc7e822d77a2dfe8b8f6136e18f1e91f1c8212019f2781164e1"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/bin/idea.sh" => "ideac")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "ideac".
+        EOS
+    end
+end
---
+++ Formula/intellij-idea.rb
@@ -0,0 +1,19 @@
+class IntellijIdea < Formula
+    desc "The Most Intelligent Java IDE, ultimate edition."
+    homepage "http://www.jetbrains.com/idea/"
+    version "2018.3.4"
+    url "https://download.jetbrains.com/idea/ideaIU-#{version}.tar.gz"
+    sha256 "3866349090ea295c0ac4f1b77d20c74d8c9647e73e5ad541c61b3dfbfc4ab5f3"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/bin/idea.sh" => "idea")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "idea".
+        EOS
+    end
+end
