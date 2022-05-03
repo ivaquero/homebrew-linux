e05df961d0e20c2be3c0d2bc330b4d6b72369b0c
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-05-03 18:17:45 +0800

    fix

--- Formula/nextcloud.rb
+++ Formula/nextcloud.rb
@@ -3,7 +3,7 @@ class Nextcloud < Formula
     version "3.4.4"
 
     url "https://github.com/nextcloud/desktop/releases/download/v#{version}/Nextcloud-#{version}-x86_64.AppImage"
-    sha256 "cc5d219d7a815e744b8371a2641256c29908a163bb2fef858252a536054a5a4f"
+    sha256 "5fce5febe1a85e3875a018bdb9581ed1e3174143eee570aa3e45014645445145"
   
     desc "A safe home for all your data"
     homepage "https://nextcloud.com"
