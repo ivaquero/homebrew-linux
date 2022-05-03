dd9da9366f1dc01898d8e401ebd3a0dd82b584e3
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-05-03 17:26:41 +0800

    update ci

--- Formula/cryptomator.rb
+++ Formula/cryptomator.rb
@@ -3,7 +3,7 @@ class Cryptomator < Formula
     version "1.6.9"
 
     url "https://github.com/cryptomator/cryptomator/releases/download/#{version}/Cryptomator-#{version}-x86_64.AppImage"
-    sha256 "333b55f78779c8ce93bace15987d546d46d05f4543b90f1b6afdbadf348921d1"
+    sha256 "a60cfd735a34ee95ea1f541dc9f3132070a0722a236368bc001e89efa0eb7fab"
 
     desc "Multi-platform client-side cloud file encryption tool"
     homepage "https://cryptomator.org/"
