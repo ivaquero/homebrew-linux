60abcd360ea374cb74cf40183c7767b93ccbd909
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-05-03 16:52:04 +0800

    add marktext

---
+++ Formula/marktext.rb
@@ -0,0 +1,22 @@
+class Marktext < Formula
+    desc "Next generation markdown editor, running on platforms of MacOS Windows and Linux."
+    homepage "https://github.com/marktext/marktext"
+    version "0.17.1"
+
+    url "https://github.com/marktext/marktext/releases/download/v#{version}/marktext-#{version}-x86_64.tar.gz"
+    sha256 "333b55f78779c8ce93bace15987d546d46d05f4543b90f1b6afdbadf348921d1"
+
+    bottle :unneeded
+    cxxstdlib_check :skip
+
+    def install
+        prefix.install Dir["*"]
+        chmod(0755, "#{prefix}/marktext-#{version}-x86_64.AppImage")
+        bin.install_symlink("#{prefix}/marktext-#{version}-x86_64.AppImage" => "marktext")
+    end
+
+    def caveats; <<~EOS
+        Executable is linked as "marktext".
+        EOS
+    end
+end
