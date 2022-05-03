48d35e6122340f598825263b6d7399a36077a710
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-05-03 18:14:24 +0800

    fix

--- Formula/cryptomator.rb
+++ Formula/cryptomator.rb
@@ -2,7 +2,7 @@ class Cryptomator < Formula
    
     version "1.6.9"
 
-    url "https://github.com/cryptomator/cryptomator/releases/download/#{version}/Cryptomator-#{version}-tar.gz"
+    url "https://github.com/cryptomator/cryptomator/releases/download/#{version}/cryptomator-#{version}-x86_64.AppImage"
     sha256 "a60cfd735a34ee95ea1f541dc9f3132070a0722a236368bc001e89efa0eb7fab"
 
     desc "Multi-platform client-side cloud file encryption tool"
@@ -10,7 +10,8 @@ class Cryptomator < Formula
 
     def install
         prefix.install Dir["*"]
-        bin.install_symlink("#{libexec}/Cryptomator" => "Cryptomator")
+        chmod(0755, "#{prefix}/cryptomator-#{version}-x86_64.AppImage")
+        bin.install_symlink("#{prefix}/cryptomator-#{version}-x86_64.AppImage" => "Cryptomator")
     end
 
     def caveats; <<~EOS
