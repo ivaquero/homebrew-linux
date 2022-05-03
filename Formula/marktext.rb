b6f9915452c2d9b78aab876a6ab33d3f322cf06c
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-05-03 18:11:11 +0800

    fix

--- Formula/marktext.rb
+++ Formula/marktext.rb
@@ -3,7 +3,7 @@ class Marktext < Formula
     version "0.17.1"
 
     url "https://github.com/marktext/marktext/releases/download/v#{version}/marktext-x64.tar.gz"
-    sha256 "2e2555113e37df830ba3958efcccce7020907b12fd4162368cfd906aeda630b7"
+    sha256 "6e333bcd1e8ae0cd93e1de52bdb966fdd9b8896986acb72f13d23ae100872e5c"
 
     desc "Next generation markdown editor, running on platforms of MacOS Windows and Linux."
     homepage "https://github.com/marktext/marktext"
